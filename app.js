9b13e42880016f79891b0813054884243d90a161
Nimira43 <n.rabone72@aol.com>
2024-12-06 23:58:41 +0000

    Transferring over HTML into app.js

--- app.js
+++ app.js
@@ -10,7 +10,44 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send(`
-    Testing
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="public/billy.png">
+  <link rel="stylesheet" href="public/style.css">
+  <title>HTMX Login</title>
+</head>
+<body>
+  <main>
+    <form>
+      <h1 class="heading">NimiraTech Login</h1>
+      <div class="control">
+        <label for="email">Email</label>
+        <input 
+          type="email" 
+          name="email" 
+          id="email"
+        >
+        <p class="error"></p>
+      </div>
+      <div class="control">
+        <label for="password">Password</label>
+        <input 
+          type="password" 
+          name="password" 
+          id="password"
+        >
+        <p class="error"></p>
+      </div>
+      <p>
+        <button type="submit">Login</button>
+      </p>
+    </form>
+  </main>
+</body>
+</html>
   `)
 })
 
