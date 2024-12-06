01bea4ab850f62a8c5631f31506cfc3d123e1335
Nimira43 <n.rabone72@aol.com>
2024-12-06 23:52:17 +0000

    Setting up Express

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from express
+
+const app = express();
+
+app.use(express.urlencoded({
+  extended: false
+}))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send(`
+    Testing
+  `)
+})
