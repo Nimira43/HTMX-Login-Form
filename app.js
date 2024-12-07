303fe5eb504125c75cc234776d31b81d90184f39
Nimira43 <n.rabone72@aol.com>
2024-12-07 00:04:18 +0000

    Tidy up of HTML code in app.js

--- app.js
+++ app.js
@@ -11,43 +11,43 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="/billy.png">
-  <link rel="stylesheet" href="/style.css">
-  <title>HTMX Login</title>
-</head>
-<body>
-  <main>
-    <form>
-      <h1 class="heading">NimiraTech Login</h1>
-      <div class="control">
-        <label for="email">Email</label>
-        <input 
-          type="email" 
-          name="email" 
-          id="email"
-        >
-        <p class="error"></p>
-      </div>
-      <div class="control">
-        <label for="password">Password</label>
-        <input 
-          type="password" 
-          name="password" 
-          id="password"
-        >
-        <p class="error"></p>
-      </div>
-      <p>
-        <button type="submit">Login</button>
-      </p>
-    </form>
-  </main>
-</body>
-</html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="icon" type="image/png" href="/billy.png">
+      <link rel="stylesheet" href="/style.css">
+      <title>HTMX Login</title>
+    </head>
+    <body>
+      <main>
+        <form>
+          <h1 class="heading">NimiraTech Login</h1>
+          <div class="control">
+            <label for="email">Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email"
+            >
+            <p class="error"></p>
+          </div>
+          <div class="control">
+            <label for="password">Password</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password"
+            >
+            <p class="error"></p>
+          </div>
+          <p>
+            <button type="submit">Login</button>
+          </p>
+        </form>
+      </main>
+    </body>
+    </html>
   `)
 })
 
